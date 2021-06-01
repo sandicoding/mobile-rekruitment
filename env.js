2d7d6849468a17e98f0eb59e7bf9df6e64e44daf
yudican <yudhichandra02@gmail.com>
2021-06-01 19:34:08 +0700

    updated redux

---
+++ env.js
@@ -0,0 +1,3 @@
+export const env = {
+  API_URL: 'https://absenkuy.yudicandra.web.id/api',
+};
---
+++ src/action/AuthAction.js
@@ -0,0 +1,66 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import {Alert, NativeModules, ToastAndroid} from 'react-native';
+import Toast from 'react-native-toast-message';
+import {env} from '../../env';
+import {setHeaderToken} from '../config/axios/setHeaderToken';
+
+import {
+  IS_LOADING,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  SET_INITIAL_ROUTE_NAME,
+  SET_IS_LOGIN,
+  SET_TOKEN,
+  SET_USER,
+} from '../config/const';
+
+export const login =
+  (data, navigation = null) =>
+  dispatch => {
+    dispatch({type: IS_LOADING});
+    axios
+      .post(`${env.API_URL}/auth/login`, data)
+      .then(async result => {
+        if (result.data) {
+          let response = result.data;
+          let token = response.data.access_token;
+          let user = response.data.user;
+
+          await AsyncStorage.setItem('access_token', token);
+
+          dispatch({type: SET_USER, payload: user});
+          dispatch({type: SET_TOKEN, payload: token});
+          dispatch({type: SET_IS_LOGIN});
+          dispatch({type: IS_LOADING});
+          setHeaderToken(token);
+          let initialRouteName = 'Home';
+
+          return navigation.navigate('CheckIfLoggin', {initialRouteName});
+        }
+      })
+      .catch(err => {
+        let result = err.response;
+        if (result?.data) {
+          ToastAndroid.showWithGravity(
+            result.data.message,
+            ToastAndroid.SHORT,
+            ToastAndroid.BOTTOM,
+          );
+        }
+      });
+  };
+
+export const logout = navigation => async dispatch => {
+  await AsyncStorage.removeItem('access_token');
+  await AsyncStorage.removeItem('user');
+  await AsyncStorage.removeItem('initialRouteName');
+
+  dispatch({type: SET_USER, payload: {}});
+  dispatch({type: SET_TOKEN, payload: null});
+  dispatch({type: LOGOUT, payload: false});
+  dispatch({type: SET_IS_LOGIN});
+  dispatch({type: SET_INITIAL_ROUTE_NAME, payload: 'Login'});
+
+  navigation.replace('Login');
+};
